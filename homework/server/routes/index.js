cbe06efb9b83a0f3b71d8c9ca94ea99946ac7eb0
elvece <lvcifferello@gmail.com>
2015-08-21 10:51:48 -0600

    added txt file for readFile homework exercise

--- homework/server/routes/index.js
+++ homework/server/routes/index.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(request, response, next) {
   var arr = [];
   http.get('http://omdbapi.com/?i=tt0241527', function(response){
     response.setEncoding().on('data', function(data){
       arr.push(data);
-      res.render('index', { title: arr });
+    });
+    response.render('index', {
+      title: "Harry Potter Homework",
+      info: arr
     });
   });
 });
 
+
 module.exports = router;
+
+// http.get('http://omdbapi.com/?i=tt0295297', function(response){
+//   response.setEncoding().on('data', function(data){
+//     arr.push(data);
+//   });
