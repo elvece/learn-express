e193cf6eecd64fca6a7ab32c2c4959689943c40e
elvece <lvcifferello@gmail.com>
2015-08-23 21:21:43 -0600

    refactored http get problem

--- homework/server/routes/index.js
+++ homework/server/routes/index.js
@@ -1,24 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+var arr = [];
 
-router.get('/', function(request, response, next) {
-  var arr = [];
-  http.get('http://omdbapi.com/?i=tt0241527', function(response){
-    response.setEncoding().on('data', function(data){
+router.get('/', function(req, res, next) {
+  //first request
+  http.get('http://omdbapi.com/?i=tt0241527', function(res){
+    res.setEncoding('utf8');
+    res.on('data', function(data){
       arr.push(data);
     });
-    response.render('index', {
-      title: "Harry Potter Homework",
-      info: arr
+    //second request
+    res.on('end', function(){
+      http.get('http://omdbapi.com/?i=tt0295297', function(response){
+        response.setEncoding('utf8');
+        response.on('data', function(data){
+          arr.push(data);
+      res.render('index', {
+        title: "Harry Potter Homework",
+        info: data
+      });
+        });
+      });
     });
   });
+  //rendering
 });
 
 
 module.exports = router;
 
-// http.get('http://omdbapi.com/?i=tt0295297', function(response){
-//   response.setEncoding().on('data', function(data){
-//     arr.push(data);
-//   });
